42f6e916ec7b81876c32778b0434849fd68480d8
Aleks <a.cikinovs@icloud.com>
2022-01-02 08:22:26 +0200

    applying Promises and prettier

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 5. Run server by typing in terminal `npm start`
 6. Compile ts files by `tsc -w`
 7. See result in the browser console
+8. Installing Prettier for the project by `npm install prettier --save-dev` and create config file `.prettierrc`
+9. Example of prettier configurations
+```
+{
+  "singleQuote": true,
+  "arrowParens": "avoid",
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": false,
+  "bracketSpacing": true
+}
+```
--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,113 @@
-const countriesContainer = document.querySelector('.countries')! as HTMLDivElement;
-
-// old way
-
+const countriesContainer = document.querySelector(
+  '.countries'
+)! as HTMLDivElement
 
 interface CountryInfo {
-    name: { common: string };
-    flags: Flags;
-    region: string;
-    population: number;
-    languages: Dictionary;
-    currencies: Dictionary;
-    borders: string[];
+  name: { common: string }
+  flags: Flags
+  region: string
+  population: number
+  languages: Dictionary
+  currencies: Dictionary
+  borders: string[]
 }
 
 export interface Flags {
-    png: string;
-    svg: string;
+  png: string
+  svg: string
 }
 
 export interface Dictionary {
-    [key: string]: { name: string };
+  [key: string]: { name: string }
 }
 
-const renderCountry = function (data: CountryInfo, className: string = ''): void {
-    const html = `
+/**
+ * Custom type - response type when getting response with country
+ */
+type CountryResponse = {
+  status: number
+  statusText: string
+  json: any
+}
+
+/**
+ * Rendering country information to the web
+ * @param data Information about country with CountryInfo interface format
+ * @param className Html class name to display neighbour country in different format
+ */
+const renderCountry = function (
+  data: CountryInfo,
+  className: string = ''
+): void {
+  const html = `
 <article class="country ${className}">
 <img class="country__img" src="${data.flags.png}"  alt="${data.name.common}"/>
 <div class="country__data">
   <h3 class="country__name">${data.name.common}</h3>
   <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} M people</p>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+    1
+  )} M people</p>
   <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
   <p class="country__row"><span>💰</span>${Object.keys(data.currencies)}</p>
 </div>
 </article>
-`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
+`
+  countriesContainer.insertAdjacentHTML('beforeend', html)
+  countriesContainer.style.opacity = '1'
 }
 
+/**
+ * Get country data without using Promises
+ * @param country Name of the country
+ */
 const getCountryData = function (country: string): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-// fetch data in background
-    request.send();
+  const request = new XMLHttpRequest()
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
+  // fetch data in background
+  request.send()
 
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText)
+    console.log(data)
 
-        // render country one
-        renderCountry(data);
+    // render country one
+    renderCountry(data)
 
-        // get neighbour counties
-        const [...neighbours] = data.borders;
-        console.log(neighbours);
+    // get neighbour counties
+    const [...neighbours] = data.borders
+    console.log(neighbours)
 
-        neighbours.forEach((value: string) => {
-            const requestX = new XMLHttpRequest();
-            requestX.open('GET', `https://restcountries.com/v3.1/alpha/${value}`);
-            requestX.send();
-            requestX.addEventListener('load', function () {
-                const [dataX] = JSON.parse(this.responseText);
-                renderCountry(dataX, 'neighbour');
-            })
+    neighbours.forEach((value: string) => {
+      const requestX = new XMLHttpRequest()
+      requestX.open('GET', `https://restcountries.com/v3.1/alpha/${value}`)
+      requestX.send()
+      requestX.addEventListener('load', function () {
+        const [dataX] = JSON.parse(this.responseText)
+        renderCountry(dataX, 'neighbour')
+      })
+    })
+  })
+}
 
-        })
+// getCountryData('Russia')
+
+/**
+ * Get Countries using Promises
+ * @param country Name of the country
+ */
+const getCountryDataPr = function (country: string) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then<CountryInfo[]>((response: CountryResponse) => response.json())
+    .then<CountryResponse>(data => {
+      renderCountry(data[0])
+      const neighbour = data[0].borders[0]
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+    })
+    .then<CountryInfo[]>((response: CountryResponse) => response.json())
+    .then(data => {
+      renderCountry(data[0], 'neighbour')
     })
 }
 
-getCountryData('Latvia')
+getCountryDataPr('Latvia')
